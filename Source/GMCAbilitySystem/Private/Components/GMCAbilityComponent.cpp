0d631f9b82dcd985e12096704331ada278c4099f
Eric Rajot <aherys@deepworlds.studio>
2024-10-08 16:50:38 +0200

    Fix crash BL-SERVER-B

--- Source/GMCAbilitySystem/Private/Components/GMCAbilityComponent.cpp
+++ Source/GMCAbilitySystem/Private/Components/GMCAbilityComponent.cpp
@@ -741,7 +741,7 @@ void UGMC_AbilitySystemComponent::TickActiveEffects(float DeltaTime)
 	for (const TPair<int, UGMCAbilityEffect*>& Effect : ActiveEffects)
 	{
 		
-		if (!IsValid(Effect.Value)) {
+		if (!IsValid(Effect.Value) || !Effect.Value->IsValidLowLevel()) {
 			UE_LOG(LogGMCAbilitySystem, Error, TEXT("Active Effect id %d is null or pending kill, removing from the list."), Effect.Key);
 			CompletedActiveEffects.Push(Effect.Key);
 			continue;	
